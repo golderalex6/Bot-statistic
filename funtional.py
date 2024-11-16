100ede05223832a127cce8cacbee0ef97110740f
HongLoan <golderalex6@gmail.com>
2024-11-17 01:58:16 +0700

    small update

---
+++ funtional.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from time import sleep
+plt.rc('figure',titleweight='bold',titlesize='large',figsize=(15,9))
+plt.rc('axes',labelweight='bold',labelsize='large',titleweight='bold',titlesize='large',grid=True)
+
+class functional():
+    pass
